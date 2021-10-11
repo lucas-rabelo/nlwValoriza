a001e5b2bee36905f21a64bfcacc8d719ba037d2
lucas-rabelo <lucasrabelodesouza8@gmail.com>
2021-10-11 16:58:35 -0300

    Servidor

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,12 @@
+import "reflect-metadata";
 import express from 'express';
+import { router } from './routes';
 
-// @types/express
+import './database';
+
 const app = express();
 
-/**
- * GET    => Busca de informações
- * POST   => Inserir ( criar ) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
+app.use(express.json());
+app.use(router);
 
-/**
- * Tipos de parâmetros:
- * 
- * Routes params => http://localhost:3000/produtos/965464984 -> id usado para a busca de algum item ou dado
- * 
- * Query params  => http://localhost:3000/produtos?name=teclado&description=teclado+bom
- * 
- * Body params   => {
- *     "name": "teclado",
- *     "desciption": "teclado bom"
- * }
- */
-
-app.get('/test', ( request, response ) => {
-    // request => Tudo que está entrando
-    // response => tudo que está saindo
-
-    return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW - método post");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log('Server is running'));
